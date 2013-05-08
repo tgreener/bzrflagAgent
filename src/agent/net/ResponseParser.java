f17749ba96f069b1639eb8b2636c8228c49d4633
Todd Greener <todd.greener@gmail.com>
2013-05-08 14:18:20 -0600

    Worked on parser

--- src/agent/net/ResponseParser.java
+++ src/agent/net/ResponseParser.java
@@ -1,11 +1,26 @@
 
 package agent.net;
 
+import java.util.Scanner;
+
 public class ResponseParser {
 	
 	public ResponseParser() {
-
 	}
 	
-	
+	public boolean checkAcknowledged(String response) {
+		Scanner scan = new Scanner(response);
+
+		return scan.hasNext("ack");
+	}
+
+	public boolean checkOk(String response) {
+		Scanner scan = new Scanner(response);
+		if(checkAcknowledged(response)) {
+			scan.nextLine();
+		}
+
+		return scan.hasNext("ok");
+	}
 }
+
