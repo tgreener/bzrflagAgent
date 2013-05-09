4e0330c7bd13fe10a7f22d3e13e0ec9cb15241bf
tgreener <todd.greener@gmail.com>
2013-05-09 13:26:11 -0600

    Updated fields

--- src/agent/fields/AttractiveRadialField.java
+++ src/agent/fields/AttractiveRadialField.java
@@ -1,9 +1,43 @@
 
 package agent.fields;
 
+import java.awt.geom.Point2D;
+
 public class AttractiveRadialField extends RadialField{
 
-	
+	public AttractiveRadialField(double a, double r, double x, double y) {
+		super(a, r, x, y);
+	}
 
+	public AttractiveRadialField(double a, double r, double s, double x, double y) {
+		super(a, r, s, x, y);
+	}
+
+	public Vector2d fieldAtPoint(Point2D p) {
+		return fieldAtPoint(p.getX(), p.getY());
+	} 
+
+	public Vector2d fieldAtPoint(double x, double y) {
+		double d = distanceToGoal(x, y);
+		double theta = getTheta(x, y);
+		double cosTheta = Math.cos(theta);
+		double sinTheta = Math.sin(theta);
+
+		double dx = 0;
+		double dy = 0;
+		
+		if(radius <= d && d <= (spread + radius)) {
+			dx = alpha * (d - radius) * cosTheta;
+			dy = alpha * (d - radius) * sinTheta;
+		}
+		else if(d > (spread + radius)){
+			dx = alpha * spread * cosTheta;
+			dy = alpha * spread * sinTheta;
+		}
+		
+		Vector2d result = new Vector2d(dx, dy);
+
+		return result;
+	} 
 }
 
--- src/agent/fields/RadialField.java
+++ src/agent/fields/RadialField.java
@@ -6,16 +6,31 @@ public abstract class RadialField extends Field{
 	protected double radius;
 	protected double spread;
 
-	public RadialField(double a, double r) {
+	protected double gx;
+	protected double gy;
+
+	public RadialField(double a, double r, double x, double y) {
 		super(a);
 		radius = r;
 		spread = Double.POSITIVE_INFINITY;
+		this.gx = x;
+		this.gy = y;
 	}
 
-	public RadialField(double a, double r, double s) {
+	public RadialField(double a, double r, double s, double x, double y) {
 		super(a);
 		radius = r;
 		spread = s;
+		this.gx = x;
+		this.gy = y;
+	}
+
+	protected double distanceToGoal(double x, double y) {
+		return (new Vector2d(x - this.gx, y - this.gy)).length();
+	}
+
+	protected double getTheta(double x, double y) {
+		return Math.atan((gy - y) / (gx - x));
 	}
 }
 
