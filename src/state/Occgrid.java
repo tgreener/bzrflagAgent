96089281f8abacfde2b8773926b9ac552d62ad4e
Todd Greener <todd.greener@gmail.com>
2013-05-14 14:15:15 -0600

    Added visitation to Occgrid state

--- src/state/Occgrid.java
+++ src/state/Occgrid.java
@@ -2,16 +2,19 @@ package state;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Occgrid {
 	
 	int[][] grid;
+	boolean[][] visitedGrid;
 	Point at;
 	int[] dimension;
 	
 	
 	public Occgrid(ArrayList<String> response, Point at, int[] dimension) {
 		int[][] grid = new int[response.size()][response.get(0).length()];
+		boolean[][] visitedGrid = new boolean[response.size()][response.get(0).length()];
 		int i = 0;
 		for(String s : response){
 			System.out.println("STRING:" + s);
@@ -21,6 +24,10 @@ public class Occgrid {
 			}
 			i++;
 		}
+		
+		for(int v = 0; v < visitedGrid.length; v++) {
+			Arrays.fill(visitedGrid[v], false);
+		}
 	}
 	
 	public int getValueAt(int x, int y){
@@ -29,6 +36,14 @@ public class Occgrid {
 	
 	public void setValueAt(int newValue, int x, int y){
 		grid[x][y] = newValue;
+	}
+
+	public boolean visited(int x, int y) {
+		return visitedGrid[x][y];
+	}
+
+	public void visit(int x, int y) {
+		visitedGrid[x][y] = true;
 	}
 	
 }
